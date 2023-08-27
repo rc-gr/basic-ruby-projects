7a79a545b44c59fea9757a20a1f6128c243d09b8
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-08-28 00:50:48 +0800

    Add proper functionality for alphabets
    It will ignore any non-alphabets, keeping them as-is.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,17 @@
 def caesar_cipher(string, shift_factor)
-  string
+  encrypted_char_array = string.chars.map do |char|
+    char_ordinal = char.ord
+    offset = 0
+    if char_ordinal.between?("A".ord, "Z".ord)
+      offset = "A".ord
+    elsif char_ordinal.between?("a".ord, "z".ord)
+      offset = "a".ord
+    end
+    
+    if offset > 0
+      char_ordinal = (char_ordinal - offset + shift_factor) % 26 +  offset
+    end
+    char_ordinal.chr
+  end
+  encrypted_char_array.join
 end
