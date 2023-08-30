45459cceef1817365e7f582dfd1d86013425f28c
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-08-30 23:20:25 +0800

    Add proper functionality

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,10 @@
 def substrings(text, dictionary)
-  {}
+  lowercase_text = text.downcase
+  dictionary.reduce(Hash.new()) do |result, substring|
+    count = lowercase_text.scan(substring.downcase).length
+    if count > 0
+      result[substring] = count
+    end
+    result
+  end
 end
