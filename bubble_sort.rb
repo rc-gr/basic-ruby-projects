d5396e515292a219bb17b86a2bc5247820055e48
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-09-02 00:16:24 +0800

    Implement bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,4 +2,18 @@ def bubble_sort(array)
   if array.length < 2
     return array
   end
+
+  passes = 0
+  sorted = false
+  until sorted || array.length - passes < 2
+    sorted = true
+    (array.length - passes - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+    end
+    passes += 1
+  end
+  array
 end
